b971a16a18c0c09ea1036e596f6afafc56cc8d53
Theodore Thuggington <thugfi@protonmail.com>
2020-12-09 17:49:27 -0500

    add cors

--- src/tickers.ts
+++ src/tickers.ts
@@ -18,6 +18,30 @@ interface ReturnShape {
   }
 }
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
 export default async function(req: NowRequest, res: NowResponse): Promise<void> {
   try {
     const pairs = await getTopPairs()
