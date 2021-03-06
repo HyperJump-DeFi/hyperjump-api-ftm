5fd53b646135a9dda35033f96d514d6a95886bac
Theodore Thuggington <thugfi@protonmail.com>
2021-03-06 17:37:11 -0500

    fix fee rate + cors

--- src/assets.ts
+++ src/assets.ts
@@ -5,14 +5,14 @@ import { getTopPairs, Pair } from './_shared'
 import { return200, return500 } from './utils/response'
 
 interface ReturnShape {
-  [tokenAddress: string]: { id: string; name: string; symbol: string; maker_fee: '0'; taker_fee: '0.003' }
+  [tokenAddress: string]: { id: string; name: string; symbol: string; maker_fee: '0'; taker_fee: '0.004' }
 }
 
 export default async function(req: NowRequest, res: NowResponse): Promise<void> {
   try {
     const pairs = await getTopPairs()
     const tokens = pairs.reduce<{
-      [tokenAddress: string]: { id: string; name: string; symbol: string; maker_fee: '0'; taker_fee: '0.003' }
+      [tokenAddress: string]: { id: string; name: string; symbol: string; maker_fee: '0'; taker_fee: '0.004' }
     }>((memo: ReturnShape, pair: Pair): ReturnShape => {
       for (let token of [pair.token0, pair.token1]) {
         const id = getAddress(token.id)
@@ -22,7 +22,7 @@ export default async function(req: NowRequest, res: NowResponse): Promise<void> 
           name: token.name,
           symbol: token.symbol,
           maker_fee: '0',
-          taker_fee: '0.003'
+          taker_fee: '0.004'
         }
       }
       return memo
