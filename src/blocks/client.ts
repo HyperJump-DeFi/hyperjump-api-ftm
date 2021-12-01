91b2aa62d79b73b2a1931790ffbcb1472c6c07c0
angry-mech <angry_mech@protonmail.com>
2021-12-01 10:39:27 -0600

    use graph bklocs subgraph

--- src/blocks/client.ts
+++ src/blocks/client.ts
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: 'https://ftm-blocks.hyperjump.app'
+    uri: 'https://api.thegraph.com/subgraphs/name/angry-mech/hyperjump-ftm-blocks' // https://ftm-blocks.hyperjump.app'
   }),
   cache: new InMemoryCache()
 })
